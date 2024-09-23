7f710ca2ab0e8b2b1ac8aa880f4550fe79ddf1d0
Haru <gabs.haru@gmail.com>
2024-09-23 18:07:57 -0400

    Método diceRoll

--- src/Dice.java
+++ src/Dice.java
@@ -1,4 +1,18 @@
+import java.util.Random;
+
 public class Dice {
+
+    // Todo dado possui os seguintes atributos:
+    int numLados;
+    Random roll = new Random();
+
+    // Método de rolagem de dado
+
+    void diceRoll() {
+
+        // Imprime o "nome" do dado concatenado com seu número de lados e a rolagem em sequência.
+        System.out.println("d" + numLados + " = " + roll.nextInt(numLados) +1);
+    }
 
 
 }
